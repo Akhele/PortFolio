52b1015c71ce00e36afc6cc077773f7da792e135
MbM <moneimb8@gmail.com>
2018-08-20 19:52:59 +0100

    idk

---
+++ index.php
@@ -0,0 +1,305 @@
+<!-- === BEGIN HEADER === -->
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+    <!--<![endif]-->
+    <head>
+        <!-- Title -->
+        <title>Akhele : Full Stack Developer</title>
+        <!-- Meta -->
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!-- Favicon -->
+        <link href="favicon.ico" rel="shortcut icon">
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css" rel="stylesheet">
+        <!-- Template CSS -->
+        <link rel="stylesheet" href="assets/css/animate.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/nexus.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/responsive.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/custom.css" rel="stylesheet">
+        <!-- Google Fonts-->
+        <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="body-bg">
+            <!-- Phone/Email -->
+            <div id="pre-header" class="background-gray-lighter">
+                <div class="container no-padding">
+                    <div class="row hidden-xs">
+                        <div class="col-sm-6 padding-vert-5">
+                            <strong>Phone:</strong>&nbsp;+212-627-883282
+                        </div>
+                        <div class="col-sm-6 text-right padding-vert-5">
+                            <strong>Email:</strong>&nbsp;moneimb8@gmail.com
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Phone/Email -->
+            <!-- Header -->
+            <div id="header">
+                <div class="container">
+                    <div class="row">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html" title="">
+                                <!-- <img src="assets/img/logo.png" alt="Logo" /> -->
+                                  <span class="logo1"><b>Akhele</b></span><br>
+                                  <span class="logo2"><i>Full-Stack Developer</i></span>
+                            </a>
+                        </div>
+                        <!-- End Logo -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Header -->
+            <!-- Top Menu -->
+            <div id="hornav" class="bottom-border-shadow">
+                <div class="container no-padding border-bottom">
+                    <div class="row">
+                        <div class="col-md-8 no-padding">
+                            <div class="visible-lg">
+                                <ul id="hornavmenu" class="nav navbar-nav">
+                                    <li>
+                                        <a href="index.html" class="fa-home active">Home</a>
+                                    </li>
+                                    <li>
+                                        <a class="fa-mobile " href="projects/mobile/Kotlin.php">About Me</a>
+                                    </li>
+                                    <li>
+                                        <a class="fa-code " href="projects/mobile/Kotlin.php">Projects</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="contact.html" class="fa-comment pull-right">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 no-padding">
+                            <ul class="social-icons pull-right">
+                                <li class="social-googleplus">
+                                    <a href="#" target="_blank" title="Google+"></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Top Menu -->
+            <!-- === END HEADER === -->
+            <!-- === BEGIN CONTENT === -->
+            <div id="slideshow" class="bottom-border-shadow">
+                <div class="container no-padding background-white bottom-border">
+                    <div class="row">
+                        <!-- Carousel Slideshow -->
+                        <div id="carousel-example" class="carousel slide" data-ride="carousel">
+                            <!-- Carousel Indicators -->
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example" data-slide-to="1"></li>
+                                <li data-target="#carousel-example" data-slide-to="2"></li>
+                            </ol>
+                            <div class="clearfix"></div>
+                            <!-- End Carousel Indicators -->
+                            <!-- Carousel Images -->
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img src="assets/img/slideshow/slide1.jpg">
+                                </div>
+                                <div class="item">
+                                    <img src="assets/img/slideshow/slide2.jpg">
+                                </div>
+                                <div class="item">
+                                    <img src="assets/img/slideshow/slide3.jpg">
+                                </div>
+                                <div class="item">
+                                    <img src="assets/img/slideshow/slide4.jpg">
+                                </div>
+                            </div>
+                            <!-- End Carousel Images -->
+                            <!-- Carousel Controls -->
+                            <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-example" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                            <!-- End Carousel Controls -->
+                        </div>
+                        <!-- End Carousel Slideshow -->
+                    </div>
+                </div>
+            </div>
+            <div id="icons" class="bottom-border-shadow">
+                <div class="container background-grey bottom-border">
+                    <div class="row padding-vert-60">
+                        <!-- Icons -->
+                        <div class="col-md-3 text-center animate swing">
+                            <i class="fa-code fa-4x color-primary animate fadeIn"></i>
+                            <i class="fa-html5 fa-4x color-primary animate fadeIn"></i>
+                            <i class="fa-css3 fa-4x color-primary animate fadeIn"></i>
+                            <h2 class="padding-top-10 animate fadeIn">Web</h2>
+                            <p class="animate fadeIn"><strong>Front-End</strong> && <strong>Back-End</strong></br>Html, CSS, Java Script, Ajax, Jquery, ASP.NET</p>
+                        </div>
+                        <div class="col-md-3 text-center animate animate swing">
+                            <i class="fa-android fa-4x color-primary animate fadeIn"></i>
+                            <i class="fa-apple fa-4x color-primary animate fadeIn"></i>
+                            <i class="fa-gamepad fa-4x color-primary animate fadeIn"></i>
+                            <h2 class="padding-top-10 animate fadeIn">Mobile</h2>
+                            <p class="animate fadeIn"><strong>Apps</strong> && <strong>Games</strong> </br> Kotlin, Buildbox, Unity</p>
+                        </div>
+                        <div class="col-md-3 text-center animate swing">
+                            <i class="fa-windows fa-4x color-primary animate fadeIn"></i>
+                            <i class="fa-linux fa-4x color-primary animate fadeIn"></i>
+                            <h2 class="padding-top-10 animate fadeIn">Desktop</h2>
+                            <p class="animate fadeIn"><strong>Windows</strong> && <strong>Gnu/Linuxs</strong> </br> C, C#, VB.Net</p>
+                        </div>
+                        <div class="col-md-3 text-center animate swing">
+                            <i class="fa-terminal fa-4x color-primary animate fadeIn"></i>
+                            <h2 class="padding-top-10 animate fadeIn">Robotic</h2>
+                            <p class="animate fadeIn"><Strong>Arduino</strong> && <strong>Raspberry Pi</strong> </br> Arduino-C, Python</p>
+                        </div>
+                        <!-- End Icons -->
+                    </div>
+                </div>
+            </div>
+            <div id="content" class="bottom-border-shadow">
+                <div class="container background-white bottom-border">
+                    <div class="row margin-vert-30">
+                        <!-- Main Text -->
+                        <div class="col-md-6">
+                            <h2>Welcome to My PortFolio</h2>
+                            <p>Bla Bla Bla</p>
+                            <p>Bla Bla Bla</p>
+                        </div>
+                        <!-- End Main Text -->
+                        <div class="col-md-6">
+                            <h3 class="padding-vert-10">Key Features</h3>
+                            <p>Why Me ?</p>
+                            <ul class="tick animate fadeInRight">
+                                <li>Responsive Design</li>
+                                <li>Built with LESS</li>
+                                <li>Font Choosers</li>
+                                <li>Replaceable Background Image</li>
+                                <li>Custom Module Widths</li>
+                                <li>All Module Extensions Included</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Portfolio -->
+
+            <!-- End Portfolio -->
+            <!-- === END CONTENT === -->
+            <!-- === BEGIN FOOTER === -->
+            <div id="base">
+                <div class="container bottom-border padding-vert-30">
+                    <div class="row">
+                        <!-- Disclaimer -->
+                        <div class="col-md-4">
+                            <h3 class="class margin-bottom-10">Disclaimer</h3>
+                            <p>All stock images on this template demo are for presentation purposes only, intended to represent a live site and are not included with the template or in any of the Joomla51 club membership plans.</p>
+                            <p>Most of the images used here are available from
+                                <a href="http://www.shutterstock.com/" target="_blank">shutterstock.com</a>. Links are provided if you wish to purchase them from their copyright owners.</p>
+                        </div>
+                        <!-- End Disclaimer -->
+                        <!-- Contact Details -->
+                        <div class="col-md-4 margin-bottom-20">
+                            <h3 class="margin-bottom-10">Contact Details</h3>
+                            <p>
+                                <span class="fa-phone">Telephone:</span>1-800-123-4567
+                                <br>
+                                <span class="fa-envelope">Email:</span>
+                                <a href="mailto:info@example.com">info@example.com</a>
+                                <br>
+                                <span class="fa-link">Website:</span>
+                                <a href="http://www.example.com">www.example.com</a>
+                            </p>
+                            <p>The Dunes, Top Road,
+                                <br>Strandhill,
+                                <br>Co. Sligo,
+                                <br>Ireland</p>
+                        </div>
+                        <!-- End Contact Details -->
+                        <!-- Sample Menu -->
+                        <div class="col-md-4 margin-bottom-20">
+                            <h3 class="margin-bottom-10">Sample Menu</h3>
+                            <ul class="menu">
+                                <li>
+                                    <a class="fa-tasks" href="#">Placerat facer possim</a>
+                                </li>
+                                <li>
+                                    <a class="fa-users" href="#">Quam nunc putamus</a>
+                                </li>
+                                <li>
+                                    <a class="fa-signal" href="#">Velit esse molestie</a>
+                                </li>
+                                <li>
+                                    <a class="fa-coffee" href="#">Nam liber tempor</a>
+                                </li>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div>
+                        <!-- End Sample Menu -->
+                    </div>
+                </div>
+            </div>
+            <!-- Footer -->
+            <div id="footer" class="background-grey">
+                <div class="container">
+                    <div class="row">
+                        <!-- Footer Menu -->
+                        <div id="footermenu" class="col-md-8">
+                            <ul class="list-unstyled list-inline">
+                                <li>
+                                    <a href="#" target="_blank">Sample Link</a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank">Sample Link</a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank">Sample Link</a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank">Sample Link</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- End Footer Menu -->
+                        <!-- Copyright -->
+                        <div id="copyright" class="col-md-4">
+                            <p class="pull-right">(c) 2014 Your Copyright Info</p>
+                        </div>
+                        <!-- End Copyright -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Footer -->
+            <!-- JS -->
+            <script type="text/javascript" src="assets/js/jquery.min.js" type="text/javascript"></script>
+            <script type="text/javascript" src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+            <script type="text/javascript" src="assets/js/scripts.js"></script>
+            <!-- Isotope - Portfolio Sorting -->
+            <script type="text/javascript" src="assets/js/jquery.isotope.js" type="text/javascript"></script>
+            <!-- Mobile Menu - Slicknav -->
+            <script type="text/javascript" src="assets/js/jquery.slicknav.js" type="text/javascript"></script>
+            <!-- Animate on Scroll-->
+            <script type="text/javascript" src="assets/js/jquery.visible.js" charset="utf-8"></script>
+            <!-- Sticky Div -->
+            <script type="text/javascript" src="assets/js/jquery.sticky.js" charset="utf-8"></script>
+            <!-- Slimbox2-->
+            <script type="text/javascript" src="assets/js/slimbox2.js" charset="utf-8"></script>
+            <!-- Modernizr -->
+            <script src="assets/js/modernizr.custom.js" type="text/javascript"></script>
+            <!-- End JS -->
+    </body>
+</html>
+<!-- === END FOOTER === -->
